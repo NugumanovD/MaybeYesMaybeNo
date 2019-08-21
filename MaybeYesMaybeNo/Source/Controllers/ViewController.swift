e91167754cc682f3cc504d3ec3f6a9f34386e2ab
Nugumanov <nd@ut.in.ua>
2019-08-21 22:52:27 +0300

    Initial commit

---
+++ MaybeYesMaybeNo/Source/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MaybeYesMaybeNo
+//
+//  Created by Nugumanov Dmitriy on 8/21/19.
+//  Copyright © 2019 Nugumanov Dmitriy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MaybeYesMaybeNo/Source/Model/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  MaybeYesMaybeNo
+//
+//  Created by Nugumanov Dmitriy on 8/21/19.
+//  Copyright © 2019 Nugumanov Dmitriy. All rights reserved.
+//
+
+import Foundation
